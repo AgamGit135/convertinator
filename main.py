9e7e1b7afe55a066390868848b8b9b29c1fc7ade
agamGit135 <agamush135@gmail.com>
2026-07-19 19:46:44 +0300

    chili is barking sadly at me

---
+++ main.py
@@ -0,0 +1,27 @@
+# outline:
+# get points
+# get p hat and q hat (ie, make center of mass 0)
+# get matrices of 2 of the points
+# use matrices to get linear transformation matrix
+# use matrix to get translation
+# return java function that multiplies matrix by input and applies the translation
+# enjoy!
+import ast
+
+print("usage: <(x1,y1) (x2,y2)>, where x2,y2 is the result of the tranformation.")
+print("3 independent points needed.")
+
+points = []
+images = []
+
+for i in range(3):
+    cur = input("point and image:")
+    cur.split()
+
+    # evaluate point and image as tuples
+    curPoint = ast.literal_eval(cur[0]) 
+    curIm = ast.literal_eval(cur[1])
+    points.append(curPoint)
+    images.append(curIm)
+
+
